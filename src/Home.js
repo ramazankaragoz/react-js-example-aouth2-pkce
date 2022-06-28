cc5d999fdfda217507f47a8dffd3eecf29a53363
ramazankaragoz <karagozramazan1991@gmail.com>
2022-06-28 11:41:11 +0300

    fix config

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import { useAuth } from 'react-oauth2-pkce';
+import React, {useEffect} from 'react';
+import {useAuth} from 'react-oauth2-pkce';
 
-const Home = props =>{
+const Home = props => {
 
-    const { authService} = useAuth()
+    const {authService} = useAuth()
 
     const login = async () => {
         authService.authorize();
     }
+
     const logout = async () => {
         authService.logout();
     }
@@ -23,23 +24,24 @@ const Home = props =>{
                 <button onClick={login}>Login</button>
             </div>
         )
-    }else {
-
-        let user=authService.getUser();
-        return (
-            <div>
-                <p>Logged in {user.name} !</p>
-                <br/>
-                <p>Email: {user.email}</p>
-                <p>Expire: {user.exp}</p>
-                <br/>
-                <p>Access Token:</p>
-                <p>{authService.getAuthTokens().access_token}</p>
-                <br/>
-                <button onClick={logout}>Logout</button>
-            </div>
-        )
+    }else{
+        console.log("Authenticated..");
     }
+
+    return (
+        <div>
+            <p>Logged in {authService.getUser().name} !</p>
+            <br/>
+            <p>Email: {authService.getUser().email}</p>
+            <p>Expire: {authService.getUser().exp}</p>
+            <br/>
+            <p>Access Token:</p>
+            <p>{authService.getAuthTokens().access_token}</p>
+            <br/>
+            <button onClick={logout}>Logout</button>
+        </div>
+    )
+
 
 }
 
